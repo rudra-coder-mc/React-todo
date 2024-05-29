ebed72d421f16fb757a29a6a471cc2589e3d0dba
atul modi <forcoding255@gmail.com>
2024-05-29 10:42:55 +0530

    implemente Update mmethod

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 function App() {
   const [todos, setTodos] = useState([]);
   const [data, setData] = useState({ title: "", description: "" });
+  const [UpdateID, setUpdateID] = useState(null);
 
   // Fetch data initially and on add
   const fetchData = async () => {
@@ -12,6 +13,39 @@ function App() {
       setTodos(response.data.data);
     } catch (error) {
       console.error("Error fetching todos:", error);
+    }
+  };
+
+  /**
+   * Updates a specific todo item using its ID and new data, then refreshes the todo list.
+   */
+  const UpdateTodo = async (e) => {
+    e.preventDefault();
+
+    // Construct the URL using UpdateID
+    let url = `http://localhost:8080/api/v1/todos/${UpdateID}`;
+
+    try {
+      // Send a PATCH request to the server with the updated data
+      const response = await axios.patch(url, data);
+
+      // On successful update, refresh the list of todos
+      fetchData();
+    } catch (error) {
+      console.error("Error Updating todos:", error);
+      if (axios.isCancel(error)) {
+        console.log("Request canceled", error.message);
+      } else {
+        console.log(
+          "Error details:",
+          error?.response.status,
+          error?.response.data
+        );
+      }
+    } finally {
+      // Reset UpdateID and data to initial states
+      setUpdateID(null);
+      setData({ title: "", description: "" });
     }
   };
 
@@ -58,15 +92,19 @@ function App() {
       console.error("Error adding todo:", error);
     }
   };
-  console.log(todos);
+  // console.log(todos);
   return (
     <>
       <div className="flex flex-col justify-start items-center w-full h-dvh">
         <div className="artboard artboard-horizontal phone-1 flex flex-col justify-center items-center m-5 bg-slate-100">
-          <form className="flex flex-col gap-4 p-4 w-full" onSubmit={handleAdd}>
+          <form
+            className="flex flex-col gap-4 p-4 w-full"
+            onSubmit={UpdateID ? UpdateTodo : handleAdd}
+          >
             <input
               className="input input-bordered w-full"
               placeholder="TODO title"
+              required
               value={data.title}
               onChange={(e) => setData({ ...data, title: e.target.value })}
             />
@@ -74,12 +112,17 @@ function App() {
               type="text"
               placeholder="TODO discription"
               className="input input-bordered w-full"
+              required
               value={data.description}
               onChange={(e) =>
                 setData({ ...data, description: e.target.value })
               }
             />
-            <input type="submit" className="btn btn-primary" value="Submit" />
+            <input
+              type="submit"
+              className="btn btn-primary"
+              value={UpdateID ? "Update" : "submit"}
+            />
           </form>
         </div>
         <div className="overflow-x-auto">
@@ -115,6 +158,18 @@ function App() {
                         value="Delete"
                         className="btn bg-red-600 text-white"
                         onClick={() => DeleteTodo(todo._id)}
+                      />
+                      <input
+                        type="button"
+                        value="Updater"
+                        className="btn bg-green-600 text-white"
+                        onClick={() => {
+                          setData({
+                            title: todo.title,
+                            description: todo.description,
+                          });
+                          setUpdateID(todo._id);
+                        }}
                       />
                     </td>
                   </tr>
