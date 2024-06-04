1be656af221382852e8bc2e4ed03379eee5af031
atul modi <forcoding255@gmail.com>
2024-06-04 10:38:24 +0530

    creaate catagory with is complite

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -43,12 +43,14 @@ function App() {
     }
   };
 
+  const [state, setState] = useState(null);
+
   const { isLoading, error, data } = useGetTodo();
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>An error has occurred: {error.message}</div>;
 
-  console.log(data.data.length);
+  // console.log(data.length);
   return (
     <>
       <div className="flex flex-col justify-start items-center w-full h-dvh bg-black text-white">
@@ -79,62 +81,128 @@ function App() {
             <Input type="submit" value={updateID ? "Update" : "Submit"} />
           </form>
         </div>
-        <Options title="all" count={data.data.length} /> 
-        <Options title="Pading" count={data.data.length} /> 
-        <Options title="Complite" count={data.data.length} /> 
+        <div className="w-full flex justify-around max-w-[600px] gap-4 my-6">
+          <Options
+            title="all"
+            count={data.length}
+            onClick={() => setState(null)}
+          />
+          <Options
+            title="Pading"
+            onClick={() => {
+              setState(false);
+            }}
+          />
+          <Options title="Complite" onClick={() => setState(true)} />
+        </div>
+
         <div className="flex justify-center overflow-auto ">
           <table className="table w-1/2 bg-gray-800">
             <tbody>
-              {data.data.map((todo) => (
-                <tr key={todo._id}>
-                  <td>
-                    <label>
-                      <input
-                        type="checkbox"
-                        className="checkbox"
-                        checked={todo.isComplete}
-                        onChange={() =>
-                          toggleTodoStatus.mutate(todo._id, {
+              {state != null &&
+                data
+                  .filter((todo) => todo.isComplete == state)
+                  .map((todo) => (
+                    <tr key={todo._id}>
+                      <td>
+                        <label>
+                          <input
+                            type="checkbox"
+                            className="checkbox"
+                            checked={todo.isComplete}
+                            onChange={() =>
+                              toggleTodoStatus.mutate(todo._id, {
+                                onSuccess: () => {
+                                  queryClient.invalidateQueries("todos");
+                                },
+                              })
+                            }
+                            disabled={toggleTodoStatus.isPending}
+                          />
+                        </label>
+                      </td>
+                      <td>{todo.title}</td>
+                      <td>{todo.description}</td>
+                      <td>
+                        <button
+                          className="btn bg-red-600 text-white"
+                          onClick={() =>
+                            deleteTodo.mutate(todo._id, {
+                              onSuccess: () => {
+                                queryClient.invalidateQueries("todos");
+                              },
+                            })
+                          }
+                          disabled={deleteTodo.isPending}
+                        >
+                          Delete
+                        </button>
+                        <button
+                          className="btn bg-green-600 text-white"
+                          onClick={() => {
+                            setTodo({
+                              title: todo.title,
+                              description: todo.description,
+                            });
+                            setUpdateID(todo._id);
+                          }}
+                        >
+                          Update
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+              {state == null &&
+                data.map((todo) => (
+                  <tr key={todo._id}>
+                    <td>
+                      <label>
+                        <input
+                          type="checkbox"
+                          className="checkbox"
+                          checked={todo.isComplete}
+                          onChange={() =>
+                            toggleTodoStatus.mutate(todo._id, {
+                              onSuccess: () => {
+                                queryClient.invalidateQueries("todos");
+                              },
+                            })
+                          }
+                          disabled={toggleTodoStatus.isPending}
+                        />
+                      </label>
+                    </td>
+                    <td>{todo.title}</td>
+                    <td>{todo.description}</td>
+                    <td>
+                      <button
+                        className="btn bg-red-600 text-white"
+                        onClick={() =>
+                          deleteTodo.mutate(todo._id, {
                             onSuccess: () => {
                               queryClient.invalidateQueries("todos");
                             },
                           })
                         }
-                        disabled={toggleTodoStatus.isPending}
-                      />
-                    </label>
-                  </td>
-                  <td>{todo.title}</td>
-                  <td>{todo.description}</td>
-                  <td>
-                    <button
-                      className="btn bg-red-600 text-white"
-                      onClick={() =>
-                        deleteTodo.mutate(todo._id, {
-                          onSuccess: () => {
-                            queryClient.invalidateQueries("todos");
-                          },
-                        })
-                      }
-                      disabled={deleteTodo.isPending}
-                    >
-                      Delete
-                    </button>
-                    <button
-                      className="btn bg-green-600 text-white"
-                      onClick={() => {
-                        setTodo({
-                          title: todo.title,
-                          description: todo.description,
-                        });
-                        setUpdateID(todo._id);
-                      }}
-                    >
-                      Update
-                    </button>
-                  </td>
-                </tr>
-              ))}
+                        disabled={deleteTodo.isPending}
+                      >
+                        Delete
+                      </button>
+                      <button
+                        className="btn bg-green-600 text-white"
+                        onClick={() => {
+                          setTodo({
+                            title: todo.title,
+                            description: todo.description,
+                          });
+                          setUpdateID(todo._id);
+                        }}
+                      >
+                        Update
+                      </button>
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
--- Frontend/src/Store/Server/query.js
+++ Frontend/src/Store/Server/query.js
@@ -2,28 +2,10 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
 const featchAll = () =>
-  axios.get("http://localhost:8080/api/v1/todos").then((res) => res.data);
-const featchPadingTodo = () =>
-  axios
-    .get("http://localhost:8080/api/v1/todos?complete=true")
-    .then((res) => res.data);
-const featchCompliteTodo = () =>
-  axios
-    .get("http://localhost:8080/api/v1/todos?complete=false")
-    .then((res) => res.data);
+  axios.get("http://localhost:8080/api/v1/todos").then((res) => res.data.data);
 
 export const useGetTodo = () =>
   useQuery({
     queryKey: ["todos"],
     queryFn: () => featchAll(),
-  });
-export const usePandingTodo = () =>
-  useQuery({
-    queryKey: ["todos"],
-    queryFn: () => featchPadingTodo,
-  });
-export const useCompliteTodo = () =>
-  useQuery({
-    queryKey: ["todos"],
-    queryFn: () => featchCompliteTodo,
   });
